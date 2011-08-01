d456b43af27cd272e93da5bcdb2db14bc381c6e6
Laurent Pellegrino <lpellegr@users.noreply.github.com>
2011-08-01 16:31:28 +0200

    Moved Files from the event-cloud-utils module to the proactive-p2p-structured-utils module

--- proactive-p2p-structured/proactive-p2p-structured-utils/src/main/java/org/objectweb/proactive/extensions/p2p/structured/utils/Files.java
+++ proactive-p2p-structured/proactive-p2p-structured-utils/src/main/java/org/objectweb/proactive/extensions/p2p/structured/utils/Files.java
@@ -14,9 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>
  **/
-package fr.inria.eventcloud.utils;
+package org.objectweb.proactive.extensions.p2p.structured.utils;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Utility class for operations on {@link File}s.
@@ -32,20 +33,24 @@ public class Files {
      *            the path to the directory to remove.
      * @return <code>true</code> is the delete has succeeded, <code>false</code>
      *         otherwise.
+     * 
+     * @throws IOException
      */
-    public static boolean deleteDirectory(File path) {
+    public static void deleteDirectory(File path) throws IOException {
         if (path.exists()) {
             File[] files = path.listFiles();
             for (File file : files) {
                 if (file.isDirectory()) {
                     Files.deleteDirectory(file);
                 } else {
-                    file.delete();
+                    if (!file.delete()) {
+                        throw new IOException("Failed to delete " + file);
+                    }
                 }
             }
-            return path.delete();
+
+            path.delete();
         }
-        return false;
     }
 
 }
