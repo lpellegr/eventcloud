680daf2a4feaafce65865e09818b4d01a51c1001
lpellegr <lpellegr@localhost>
2011-09-13 18:02:35 +0200

    Updated launcher scripts to have the possibility to specify the port number to use

--- event-cloud/event-cloud-deployment/event-cloud-deployment-cli/src/main/scripts/eventcloud-launcher.sh
+++ event-cloud/event-cloud-deployment/event-cloud-deployment-cli/src/main/scripts/eventcloud-launcher.sh
@@ -2,12 +2,17 @@
 
 . $(dirname $0)/configuration/set-environment.sh
 
+PORT=$1
+shift
+
 java -Xms256m -Xmx1024m \
      -server \
      -Djava.security.policy=$PATH_TO_RESOURCES/proactive.security.policy \
      -Deventcloud.bundle.home=$PATH_TO_RESOURCES/../ \
      -Dlog4j.configuration=file:$PATH_TO_RESOURCES/log4j.properties \
      -Dlogback.configurationFile=file:$PATH_TO_RESOURCES/logback.xml \
+     -Dproactive.communication.protocol=pnp \
+     -Dproactive.pnp.port=$PORT \
      -cp $CLASSPATH fr.inria.eventcloud.deployment.cli.launchers.EventCloudLauncher $@
 
 . $(dirname $0)/configuration/unset-environment.sh
--- event-cloud/event-cloud-deployment/event-cloud-deployment-cli/src/main/scripts/eventclouds-registry-launcher.sh
+++ event-cloud/event-cloud-deployment/event-cloud-deployment-cli/src/main/scripts/eventclouds-registry-launcher.sh
@@ -2,6 +2,9 @@
 
 . $(dirname $0)/configuration/set-environment.sh
 
+PORT=$1
+shift
+
 java -Xms128m -Xmx256m \
      -server \
      -Djava.security.policy=$PATH_TO_RESOURCES/proactive.security.policy \
@@ -9,7 +12,7 @@ java -Xms128m -Xmx256m \
      -Dlog4j.configuration=file:$PATH_TO_RESOURCES/log4j.properties \
      -Dlogback.configurationFile=file:$PATH_TO_RESOURCES/logback.xml \
      -Dproactive.communication.protocol=pnp \
-     -Dproactive.pnp.port=1100 \
+     -Dproactive.pnp.port=$PORT \
      -cp $CLASSPATH fr.inria.eventcloud.deployment.cli.launchers.EventCloudsRegistryLauncher $@
 
 . $(dirname $0)/configuration/unset-environment.sh
