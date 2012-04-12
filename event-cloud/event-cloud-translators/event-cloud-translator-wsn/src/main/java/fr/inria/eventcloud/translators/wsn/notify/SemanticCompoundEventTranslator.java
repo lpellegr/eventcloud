8ff7f79bb4dbc8bd69bc22aa9610642b6699b45c
Laurent Pellegrino <lpellegr@users.noreply.github.com>
2012-04-12 16:10:45 +0200

    Fixed multiple copy of objects during translation and simplified code by using PLAY commons library

--- event-cloud/event-cloud-translators/event-cloud-translator-wsn/src/main/java/fr/inria/eventcloud/translators/wsn/notify/SemanticCompoundEventTranslator.java
+++ event-cloud/event-cloud-translators/event-cloud-translator-wsn/src/main/java/fr/inria/eventcloud/translators/wsn/notify/SemanticCompoundEventTranslator.java
@@ -17,18 +17,11 @@
 package fr.inria.eventcloud.translators.wsn.notify;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.apache.commons.lang.StringEscapeUtils;
 import org.oasis_open.docs.wsn.b_2.NotificationMessageHolderType;
 import org.oasis_open.docs.wsn.b_2.NotificationMessageHolderType.Message;
 import org.openjena.riot.WebContent;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import eu.play_project.play_commons.eventformat.EventFormatHelpers;
@@ -44,23 +37,10 @@ import fr.inria.eventcloud.translators.wsn.Translator;
  * Message Payload.
  * 
  * @author ialshaba
+ * @author lpellegr
  */
 public class SemanticCompoundEventTranslator extends
         Translator<CompoundEvent, NotificationMessageHolderType> {
-
-    // used to have the possibility to create DOM elements
-    private static Document document;
-
-    static {
-        DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = null;
-        try {
-            docBuilder = dbfac.newDocumentBuilder();
-            document = docBuilder.newDocument();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Translates the specified event to its corresponding notification message
@@ -75,30 +55,14 @@ public class SemanticCompoundEventTranslator extends
             throws TranslationException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         List<Quadruple> quads = event.getQuadruples();
-        RdfSerializer.triGWriter(out, quads);
-        Message message = new Message();
 
-        // escape XML characters before sending message
-        byte[] buf = StringEscapeUtils.escapeXml(out.toString()).getBytes();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try {
-            baos.write(buf);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        RdfSerializer.triGWriter(out, quads);
 
         Element any =
-                document.createElementNS(
-                        EventFormatHelpers.WSN_MSG_ELEMENT.getNamespaceURI(),
-                        EventFormatHelpers.WSN_MSG_ELEMENT.getPrefix()
-                                + ":"
-                                + EventFormatHelpers.WSN_MSG_ELEMENT.getLocalPart());
-        any.setTextContent(baos.toString());
-        any.setAttributeNS(
-                EventFormatHelpers.WSN_MSG_ELEMENT.getNamespaceURI(),
-                EventFormatHelpers.WSN_MSG_ELEMENT.getPrefix() + ":"
-                        + EventFormatHelpers.WSN_MSG_SYNTAX_ATTRIBUTE,
-                WebContent.contentTypeTriGAlt);
+                EventFormatHelpers.wrapWithDomNativeMessageElement(new String(
+                        out.toByteArray()), WebContent.contentTypeTriGAlt);
+
+        Message message = new Message();
         message.setAny(any);
         NotificationMessageHolderType notificationMessage =
                 new NotificationMessageHolderType();
