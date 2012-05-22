0c6346699a2338bac62b9424bed8af7d11ecfcfc
bsauvan <bsauvan@localhost>
2012-05-22 23:47:35 +0200

    Add test with social filter.

--- event-cloud/event-cloud-webservices/event-cloud-webservices-core/src/test/java/fr/inria/eventcloud/webservices/pubsub/PubSubTest.java
+++ event-cloud/event-cloud-webservices/event-cloud-webservices-core/src/test/java/fr/inria/eventcloud/webservices/pubsub/PubSubTest.java
@@ -20,11 +20,17 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.cxf.endpoint.Server;
+import org.etsi.uri.gcm.util.GCM;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.oasis_open.docs.wsn.b_2.Notify;
 import org.oasis_open.docs.wsn.b_2.Subscribe;
 import org.oasis_open.docs.wsn.bw_2.NotificationConsumer;
 import org.oasis_open.docs.wsn.bw_2.NotificationProducer;
+import org.objectweb.fractal.api.Interface;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,13 +39,20 @@ import fr.inria.eventcloud.api.CompoundEvent;
 import fr.inria.eventcloud.api.EventCloudId;
 import fr.inria.eventcloud.api.Quadruple;
 import fr.inria.eventcloud.api.Quadruple.SerializationFormat;
+import fr.inria.eventcloud.configuration.EventCloudProperties;
 import fr.inria.eventcloud.deployment.JunitEventCloudInfrastructureDeployer;
 import fr.inria.eventcloud.parsers.RdfParser;
 import fr.inria.eventcloud.translators.wsn.WsNotificationMessageBuilder;
 import fr.inria.eventcloud.utils.Callback;
+import fr.inria.eventcloud.webservices.deployment.ServiceInformation;
 import fr.inria.eventcloud.webservices.deployment.WebServiceDeployer;
 import fr.inria.eventcloud.webservices.factories.WsClientFactory;
 import fr.inria.eventcloud.webservices.services.SubscriberServiceImpl;
+import fr.insa.liris.soceda.socialfilter.relationshipstrengthengine.RelationshipStrengthEngine;
+import fr.insa.liris.soceda.socialfilter.relationshipstrengthengine.RelationshipStrengthEngineManager;
+import fr.insa.liris.soceda.socialfilter.relationshipstrengthengine.RelationshipStrengthEngineManagerAttributeController;
+import fr.insa.liris.soceda.socialfilter.relationshipstrengthengine.RelationshipStrengthEngineManagerFactory;
+import fr.insa.liris.soceda.socialfilter.socialnetwork.SocialNetwork;
 
 /**
  * Class used to test a subscribe proxy component and a publish proxy component
@@ -52,97 +65,218 @@ public class PubSubTest {
 
     private static final Logger log = LoggerFactory.getLogger(PubSubTest.class);
 
+    private JunitEventCloudInfrastructureDeployer deployer;
+
+    private EventCloudId ecId;
+
+    private ServiceInformation subscribeServiceInformation;
+
+    private ServiceInformation publishServiceInformation;
+
+    private NotificationProducer subscribeClient;
+
+    private NotificationConsumer publishClient;
+
+    private SubscriberServiceImpl subscriberService;
+
+    private Server subscriberWsServer;
+
+    private String subscriberWsEndpointUrl;
+
+    @Before
+    public void init() {
+        EventCloudProperties.SOCIAL_FILTER_URL.setValue(null);
+    }
+
     @Test(timeout = 180000)
     public void testPublishSubscribeWsProxies() throws Exception {
-        JunitEventCloudInfrastructureDeployer deployer =
-                new JunitEventCloudInfrastructureDeployer();
+        // Initializes the Event Cloud, proxies and web service clients
+        this.initEventCloudAndProxiesAndClients();
 
-        EventCloudId ecId =
-                deployer.newEventCloud(
-                        new EventCloudDescription(
-                                "http://streams.event-processing.org/ids/TaxiUc"),
-                        1, 10);
+        // Creates the subscriber web service
+        this.createSubscriberWebService();
 
-        // Web services which are deployed
-        String subscribeWsUrl =
-                WebServiceDeployer.deploySubscribeWebService(
-                        deployer.getEventCloudsRegistryUrl(),
-                        ecId.getStreamUrl(), "subscribe", 8889)
-                        .getEndpoint()
-                        .getEndpointInfo()
-                        .getAddress();
+        int lastIndexOfSlash = this.ecId.getStreamUrl().lastIndexOf('/');
 
-        String publishWsUrl =
-                WebServiceDeployer.deployPublishWebService(
-                        deployer.getEventCloudsRegistryUrl(),
-                        ecId.getStreamUrl(), "publish", 8890)
-                        .getEndpoint()
-                        .getEndpointInfo()
-                        .getAddress();
-
-        SubscriberServiceImpl subscriberService = new SubscriberServiceImpl();
-        String subscriberWsUrl =
-                WebServiceDeployer.deployWebService(
-                        subscriberService, "subscriber", 8891)
-                        .getEndpoint()
-                        .getEndpointInfo()
-                        .getAddress();
-
-        // Clients associated to Web services
-        NotificationProducer subscribeClient =
-                WsClientFactory.createWsClient(
-                        NotificationProducer.class, subscribeWsUrl);
-
-        NotificationConsumer publishClient =
-                WsClientFactory.createWsClient(
-                        NotificationConsumer.class, publishWsUrl);
-
-        int lastIndexOfSlash = ecId.getStreamUrl().lastIndexOf('/');
-
-        // Extract topic information
+        // Extracts topic information
         String topicNamespace =
-                ecId.getStreamUrl().substring(0, lastIndexOfSlash + 1);
+                this.ecId.getStreamUrl().substring(0, lastIndexOfSlash + 1);
         String topicNsPrefix = "s";
         String topicLocalPart =
-                ecId.getStreamUrl().substring(lastIndexOfSlash + 1);
+                this.ecId.getStreamUrl().substring(lastIndexOfSlash + 1);
 
         // Creates the subscribe request
         Subscribe subscribeRequest =
                 WsNotificationMessageBuilder.createSubscribeMessage(
-                        subscriberWsUrl, topicNamespace, topicNsPrefix,
-                        topicLocalPart);
+                        this.subscriberWsEndpointUrl, topicNamespace,
+                        topicNsPrefix, topicLocalPart);
 
         // Subscribes for any events with topic TaxiUc
-        subscribeClient.subscribe(subscribeRequest);
+        this.subscribeClient.subscribe(subscribeRequest);
 
         // Creates the notify request
         Notify notifyRequest =
                 WsNotificationMessageBuilder.createNotifyMessage(
                         topicNamespace, topicNsPrefix, topicLocalPart,
-                        new CompoundEvent(read(
+                        new CompoundEvent(this.read(
                                 "/notification-01.trig",
-                                SerializationFormat.TriG)));
+                                SerializationFormat.TriG, null)));
 
-        publishClient.notify(notifyRequest);
+        // Publishes the event
+        this.publishClient.notify(notifyRequest);
 
-        synchronized (subscriberService.eventsReceived) {
-            while (subscriberService.eventsReceived.size() != 1) {
-                subscriberService.eventsReceived.wait();
+        synchronized (this.subscriberService.eventsReceived) {
+            while (this.subscriberService.eventsReceived.size() != 1) {
+                this.subscriberService.eventsReceived.wait();
+            }
+        }
+
+        log.info("Compound event received!");
+    }
+
+    @Test(timeout = 180000)
+    public void testPublishSubscribeWsProxiesWithSocialFilter()
+            throws Exception {
+        // Creates the subscriber web service
+        this.createSubscriberWebService();
+
+        // Creates and initializes the social filter
+        String source1 = "http://127.0.0.1:8891/source1";
+        String source2 = "http://127.0.0.1:8891/source2";
+        SocialNetwork socialNetwork = new SocialNetwork();
+        socialNetwork.add_node(source1);
+        socialNetwork.add_node(source2);
+        socialNetwork.add_node(this.subscriberWsEndpointUrl);
+        socialNetwork.add_relationship(source1, this.subscriberWsEndpointUrl);
+        socialNetwork.add_relationship(source2, this.subscriberWsEndpointUrl);
+        socialNetwork.get_relationship(source1, this.subscriberWsEndpointUrl)
+                .set_trust((float) 0.8);
+        socialNetwork.get_relationship(source2, this.subscriberWsEndpointUrl)
+                .set_trust((float) 0.4);
+        RelationshipStrengthEngineManager socialFilter =
+                RelationshipStrengthEngineManagerFactory.newRelationshipStrengthEngineManager(new RelationshipStrengthEngine(
+                        socialNetwork));
+        String socialFilterUri =
+                ((RelationshipStrengthEngineManagerAttributeController) GCM.getAttributeController(((Interface) socialFilter).getFcItfOwner())).getComponentURI();
+        EventCloudProperties.SOCIAL_FILTER_URL.setValue(socialFilterUri);
+
+        // Initializes the Event Cloud, proxies and web service clients
+        this.initEventCloudAndProxiesAndClients();
+
+        int lastIndexOfSlash = this.ecId.getStreamUrl().lastIndexOf('/');
+
+        // Extracts topic information
+        String topicNamespace =
+                this.ecId.getStreamUrl().substring(0, lastIndexOfSlash + 1);
+        String topicNsPrefix = "s";
+        String topicLocalPart =
+                this.ecId.getStreamUrl().substring(lastIndexOfSlash + 1);
+
+        // Creates the subscribe request
+        Subscribe subscribeRequest =
+                WsNotificationMessageBuilder.createSubscribeMessage(
+                        this.subscriberWsEndpointUrl, topicNamespace,
+                        topicNsPrefix, topicLocalPart);
+
+        // Subscribes for any events with topic TaxiUc
+        this.subscribeClient.subscribe(subscribeRequest);
+
+        // Creates the notify request emitted by source1
+        Notify notifyRequest =
+                WsNotificationMessageBuilder.createNotifyMessage(
+                        topicNamespace, topicNsPrefix, topicLocalPart,
+                        new CompoundEvent(this.read(
+                                "/notification-01.trig",
+                                SerializationFormat.TriG, source1)));
+
+        // Publishes the event
+        this.publishClient.notify(notifyRequest);
+
+        synchronized (this.subscriberService.eventsReceived) {
+            while (this.subscriberService.eventsReceived.size() != 1) {
+                this.subscriberService.eventsReceived.wait();
             }
         }
 
         log.info("Compound event received!");
 
-        deployer.undeploy();
+        // Creates the notify request emitted by source2
+        notifyRequest =
+                WsNotificationMessageBuilder.createNotifyMessage(
+                        topicNamespace, topicNsPrefix, topicLocalPart,
+                        new CompoundEvent(this.read(
+                                "/notification-01.trig",
+                                SerializationFormat.TriG, source2)));
+
+        // Publishes the event
+        this.publishClient.notify(notifyRequest);
+
+        synchronized (this.subscriberService.eventsReceived) {
+            this.subscriberService.eventsReceived.wait(4000);
+            Assert.assertTrue(this.subscriberService.eventsReceived.size() == 1);
+        }
     }
 
-    private static List<Quadruple> read(String file, SerializationFormat format) {
+    private void initEventCloudAndProxiesAndClients() {
+        this.deployer = new JunitEventCloudInfrastructureDeployer();
+
+        this.ecId =
+                this.deployer.newEventCloud(
+                        new EventCloudDescription(
+                                "http://streams.event-processing.org/ids/TaxiUc"),
+                        1, 10);
+
+        // Web services which are deployed
+        this.subscribeServiceInformation =
+                WebServiceDeployer.deploySubscribeWebService(
+                        this.deployer.getEventCloudsRegistryUrl(),
+                        this.ecId.getStreamUrl(), "subscribe", 8889);
+
+        this.publishServiceInformation =
+                WebServiceDeployer.deployPublishWebService(
+                        this.deployer.getEventCloudsRegistryUrl(),
+                        this.ecId.getStreamUrl(), "publish", 8890);
+
+        // Clients associated to Web services
+        this.subscribeClient =
+                WsClientFactory.createWsClient(
+                        NotificationProducer.class,
+                        this.subscribeServiceInformation.getServer()
+                                .getEndpoint()
+                                .getEndpointInfo()
+                                .getAddress());
+
+        this.publishClient =
+                WsClientFactory.createWsClient(
+                        NotificationConsumer.class,
+                        this.publishServiceInformation.getServer()
+                                .getEndpoint()
+                                .getEndpointInfo()
+                                .getAddress());
+    }
+
+    private void createSubscriberWebService() {
+        this.subscriberService = new SubscriberServiceImpl();
+        this.subscriberWsServer =
+                WebServiceDeployer.deployWebService(
+                        this.subscriberService, "subscriber", 8891);
+        this.subscriberWsEndpointUrl =
+                this.subscriberWsServer.getEndpoint()
+                        .getEndpointInfo()
+                        .getAddress();
+    }
+
+    private List<Quadruple> read(String file, SerializationFormat format,
+                                 final String publicationSource) {
         final List<Quadruple> quadruples = new ArrayList<Quadruple>();
 
         RdfParser.parse(
-                inputStreamFrom(file), format, new Callback<Quadruple>() {
+                this.inputStreamFrom(file), format, new Callback<Quadruple>() {
                     @Override
                     public void execute(Quadruple quad) {
+                        if (publicationSource != null) {
+                            quad.setPublicationSource(publicationSource);
+                        }
                         quadruples.add(quad);
                     }
 
@@ -151,7 +285,7 @@ public class PubSubTest {
         return quadruples;
     }
 
-    private static InputStream inputStreamFrom(String file) {
+    private InputStream inputStreamFrom(String file) {
         InputStream is = null;
 
         if (file != null) {
@@ -159,6 +293,14 @@ public class PubSubTest {
         }
 
         return is;
+    }
+
+    @After
+    public void tearDown() {
+        this.subscribeServiceInformation.destroy();
+        this.publishServiceInformation.destroy();
+        this.deployer.undeploy();
+        this.subscriberWsServer.destroy();
     }
 
 }
