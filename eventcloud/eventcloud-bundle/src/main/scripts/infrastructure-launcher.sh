3166ab5960bd56fe6c6177ddeb1b45ce9ad82190
Laurent Pellegrino <lpellegr@users.noreply.github.com>
2012-08-12 18:27:40 +0200

    Updated infrastructure-launcher script to make it work on Mac Os X. The problem was the inotify library which is available only with a linux kernel. The script now uses watchdog, a python library to redirect monitoring operations to the underlying library supported by the operating system.
    - Some unecessary scripts have been removed
    - Added a new option to the infrastructure-launcher script
    - Fixed the proactive java policy that was not correctly loaded

--- eventcloud/eventcloud-bundle/src/main/scripts/infrastructure-launcher.sh
+++ eventcloud/eventcloud-bundle/src/main/scripts/infrastructure-launcher.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 . $(dirname $0)/configuration/set-environment.sh
 
 # Paths to output files
-OUTPUTS_DIR=$BUNDLE_HOME/logs
-REGISTRY_OUTPUT_FILE=$OUTPUTS_DIR/eventclouds-registry.output
+LOGS_DIR=$BUNDLE_HOME/logs
+REGISTRY_OUTPUT_FILE=$LOGS_DIR/eventclouds-registry.output
 
 # Paths to instance files
 INSTANCES_DIR=$BUNDLE_HOME/instances
@@ -18,206 +18,163 @@ PROXIES_PORTS_LOWER_BOUND=9000
 WS_EVENTCLOUDS_MANAGEMENT_HTTP_PORT=8082
 WS_EVENTCLOUDS_MANAGEMENT_PNP_PORT=8083
 
-# Infrastructure scale
-NB_EVENTCLOUDS=1
-NB_PEERS_BY_EVENTCLOUD=1
-NB_TRACKERS_BY_EVENTCLOUD=1
-
-trap undeploy 1 2 15
+trap clear 1 2 15
 
 function main() {
-	cd $BUNDLE_HOME
+    cd $BUNDLE_HOME
 
-    if [[ "$(whereis inotifywait)" == "inotifywait:" ]];
-        then echo "Library 'inotify-tools' is required, please install it.";
-    fi;
+    check_requirements
    
-    if [[ $# -ne 2 && $1 = "-k" ]];
+    if [[ $# -eq 1 ]]
     then
-        undeploy
-	return 0;
+        if [[ $1 = "-k" ]]
+        then
+            undeploy
+            return 0;
+        elif [[ $1 = "-kc" ]]
+        then
+            undeploy
+            remove_logs
+            return 0
+        else
+            print_usage
+        fi
     fi;
 
     if [[ -d $INSTANCES_DIR ]];
     then
-	echo "Eventclouds registry already running. Use -k option to kill all existing instances."
-	return 1;
+	    echo "Eventclouds registry already running. Use -k option to kill all existing instances."
+	    return 1;
     fi;
 
-    mkdir -p $INSTANCES_DIR;
-    mkdir -p $OUTPUTS_DIR;
+    mkdir -p $INSTANCES_DIR &> /dev/null
+    mkdir -p $LOGS_DIR &> /dev/null
+    
+    deploy
+}
 
+function check_requirements() {
+    if ! command_exists watchmedo;
+    then
+        print_requirements
+        exit 1
+    fi 
+}
+
+function print_requirements() {
+    echo "Please install the watchdog library by using *one* of the following commands:"
+    echo "  sudo easy_install watchdog"
+    echo "  sudo pip install watchdog"
+}
+
+function print_usage() {
+    echo "usage: infrastructure-launcher.sh [-k|-kc]"
+    echo "       -k  kills the running instance"
+    echo "       -kc kills the running instance and removes the log files"
+    exit 1
+}
+
+function command_exists() {
+    return $(type $1 &> /dev/null)
+}
+
+function deploy() {
     # deploy the eventclouds registry
     deploy_eventclouds_registry
 
     # deploy eventclouds management web service
     deploy_ws_management_proxy
     
-    # deploy the event clouds
-    #deploy_eventclouds
-    
-    # deploy the ws proxies
-    #deploy_ws_proxies
+    echo "Logs available at:"
+    echo "    $LOGS_DIR"
 }
 
 function deploy_eventclouds_registry() {
-    . $BUNDLE_HOME/scripts/eventclouds-registry-launcher.sh \
-        $REGISTRY_INSTANCE_FILE $EVENTCLOUDS_REGISTRY_PORT &> $REGISTRY_OUTPUT_FILE &
+	java -Xms128m -Xmx256m \
+     -server \
+     -Djava.security.policy=$PATH_TO_RESOURCES/proactive.security.policy \
+     -Deventcloud.bundle.home=$BUNDLE_HOME \
+     -Deventcloud.configuration=$PATH_TO_RESOURCES/eventcloud.properties \
+     -Deventcloud.instance.file=$REGISTRY_INSTANCE_FILE \
+     -Dlog4j.configuration=file:$PATH_TO_RESOURCES/log4j.properties \
+     -Dlogback.configurationFile=file:$PATH_TO_RESOURCES/logback.xml \
+     -Dlogging.output.filename=$(basename $REGISTRY_INSTANCE_FILE) \
+     -Dproactive.communication.protocol=pnp \
+     -Dproactive.pnp.port=$EVENTCLOUDS_REGISTRY_PORT \
+     -cp $CLASSPATH \
+     fr.inria.eventcloud.deployment.cli.launchers.EventCloudsRegistryLauncher &> $REGISTRY_OUTPUT_FILE & 
+     
+    echo $! > $REGISTRY_INSTANCE_FILE.pid
 
     wait_file_creation $INSTANCES_DIR $(basename $REGISTRY_INSTANCE_FILE)
 
     EVENTCLOUDS_REGISTRY_URL=$(cat $REGISTRY_INSTANCE_FILE)
 
     echo "Eventclouds registry deployed at:"
-    echo $EVENTCLOUDS_REGISTRY_URL
-    echo
-}
-
-function deploy_eventclouds() {
-    PNP_PORT=$EVENTCLOUDS_PORTS_LOWER_BOUND
-
-    for ((i=1; i<=$NB_EVENTCLOUDS; i++));
-    do
-	EVENTCLOUD_INSTANCE_FILE=$(eventcloud_instance_file $i)
-
-	. $BUNDLE_HOME/scripts/eventcloud-launcher.sh $EVENTCLOUD_INSTANCE_FILE $PNP_PORT \
-            --registry-url $EVENTCLOUDS_REGISTRY_URL --nb-trackers $NB_TRACKERS_BY_EVENTCLOUD \
-            --nb-peers $NB_PEERS_BY_EVENTCLOUD &> $(eventcloud_output_file $i) &
-	
-	wait_file_creation $INSTANCES_DIR $(basename $EVENTCLOUD_INSTANCE_FILE)
-
-	PNP_PORT=$(expr $PNP_PORT + 1)
-	
-	echo "Eventcloud with $NB_TRACKERS_BY_EVENTCLOUD tracker(s) and $NB_PEERS_BY_EVENTCLOUD peer(s) is running under url:"
-	echo $(cut -d " " -f 3 $EVENTCLOUD_INSTANCE_FILE)
-    done;
-    echo
+    echo "    $EVENTCLOUDS_REGISTRY_URL"
 }
 
 function deploy_ws_management_proxy() {
-   WS_EVENTCLOUDS_MANAGEMENT_INSTANCE_FILE=$INSTANCES_DIR/ws-eventclouds-management
+    WS_EVENTCLOUDS_MANAGEMENT_INSTANCE_FILE=$INSTANCES_DIR/ws-eventclouds-management
 
-    . $BUNDLE_HOME/scripts/ws-eventclouds-management-launcher.sh \
-        $WS_EVENTCLOUDS_MANAGEMENT_INSTANCE_FILE $WS_EVENTCLOUDS_MANAGEMENT_PNP_PORT \
-        --registry-url $EVENTCLOUDS_REGISTRY_URL --port-lower-bound $PROXIES_PORTS_LOWER_BOUND \
-        -p $WS_EVENTCLOUDS_MANAGEMENT_HTTP_PORT &> $OUTPUTS_DIR/ws-eventclouds-management.output &
+    java -Xms256m -Xmx10240m \
+     -server \
+     -Djava.security.policy=$PATH_TO_RESOURCES/proactive.security.policy \
+     -Deventcloud.bundle.home=$BUNDLE_HOME \
+     -Deventcloud.configuration=$PATH_TO_RESOURCES/eventcloud.properties \
+     -Deventcloud.instance.file=$WS_EVENTCLOUDS_MANAGEMENT_INSTANCE_FILE \
+     -Dlog4j.configuration=file:$PATH_TO_RESOURCES/log4j.properties \
+     -Dlogback.configurationFile=file:$PATH_TO_RESOURCES/logback.xml \
+     -Dlogging.output.filename=$(basename $WS_EVENTCLOUDS_MANAGEMENT_INSTANCE_FILE) \
+     -Dproactive.communication.protocol=pnp \
+     -Dproactive.pnp.port=$WS_EVENTCLOUDS_MANAGEMENT_PNP_PORT \
+     -cp $CLASSPATH fr.inria.eventcloud.deployment.cli.launchers.EventCloudManagementWsLaucher \
+     --registry-url $EVENTCLOUDS_REGISTRY_URL --port-lower-bound $PROXIES_PORTS_LOWER_BOUND \
+     -p $WS_EVENTCLOUDS_MANAGEMENT_HTTP_PORT &> $LOGS_DIR/ws-eventclouds-management.output &
+
+    echo $! > $WS_EVENTCLOUDS_MANAGEMENT_INSTANCE_FILE.pid
         
-    wait_file_creation $INSTANCES_DIR $(basename $INSTANCES_DIR/ws-eventclouds-management)
+    wait_file_creation $INSTANCES_DIR $(basename $WS_EVENTCLOUDS_MANAGEMENT_INSTANCE_FILE)
     
     echo "Eventclouds management web service deployed at:"
-    echo $(cat $INSTANCES_DIR/ws-eventclouds-management)
-}
-
-function deploy_ws_proxies() {
-    PORT=$PROXIES_PORTS_LOWER_BOUND
-
-    echo "Proxies deployed are:"
-    for ((i=1; i<=NB_EVENTCLOUDS; i++))
-    do 
-	ID=$(cut -d " " -f 3 $(eventcloud_instance_file $i))
-
-	echo "  . for eventcloud $ID:"
-	
-	WS_SUBSCRIBE_INSTANCE_FILE=$(ws_instance_file ws-subscribe-proxy $i)
-	deploy_ws_proxy ws-subscribe-proxy $PORT $i $ID
-	PORT=$(expr $PORT + 1)
-	wait_file_creation $INSTANCES_DIR $(basename $WS_SUBSCRIBE_INSTANCE_FILE)
-	echo "      $(cat $WS_SUBSCRIBE_INSTANCE_FILE)"
-
-	WS_PUBLISH_INSTANCE_FILE=$(ws_instance_file ws-publish-proxy $i)
-	deploy_ws_proxy ws-publish-proxy $PORT $i $ID
-	PORT=$(expr $PORT + 1)
-	wait_file_creation $INSTANCES_DIR $(basename $WS_PUBLISH_INSTANCE_FILE)
-	echo "      $(cat $WS_PUBLISH_INSTANCE_FILE)"
-
-	WS_PUTGET_INSTANCE_FILE=$(ws_instance_file ws-putget-proxy $i)
-	deploy_ws_proxy ws-putget-proxy $PORT $i $ID
-	PORT=$(expr $PORT + 4)
-	wait_file_creation $INSTANCES_DIR $(basename $WS_PUTGET_INSTANCE_FILE)
-	echo "      $(cat $WS_PUTGET_INSTANCE_FILE)"
-    done;
-}
-
-# $1 webservice name
-# $2 webservice port number
-# $3 eventcloud index associated
-# $4 eventcloud url
-function deploy_ws_proxy() {
-    HTTP_PORT=$2
-    PNP_PORT=$(expr $2 + 3)
-    WS_INSTANCE_FILE=$(ws_instance_file $1 $3)
-
-    . $BUNDLE_HOME/scripts/$1-launcher.sh $WS_INSTANCE_FILE $PNP_PORT $HTTP_PORT \
-        --registry-url $EVENTCLOUDS_REGISTRY_URL --eventcloud-url $4 -p $HTTP_PORT &> $(ws_output_file $1 $3) &
-}
-
-# $1 is eventcloud index (1, 2, ...)
-function eventcloud_instance_file() {
-    echo "$INSTANCES_DIR/eventcloud$1"
-}
-
-# $1 is eventcloud index (1, 2, ...) 
-function eventcloud_output_file() {
-    echo "$OUTPUTS_DIR/eventcloud$1.output"
-}
-
-# $1 is webservice name 
-# $2 is eventcloud index associated to webservice (1, 2, ...)
-function ws_instance_file() {
-    echo "$INSTANCES_DIR/$1$2"
-}
-
-# $1 is webservice name 
-# $2 is eventcloud index associated to webservice (1, 2, ...)
-function ws_output_file() {
-    echo "$OUTPUTS_DIR/$1$2.output"
-}
-
-# $1 is an eventcloud URL
-function remove_stream_prefix() {
-    #echo $1 | sed s#http://streams.play-project.eu/#id#
-    echo $(basename $1)
+    echo "    $(cat $WS_EVENTCLOUDS_MANAGEMENT_INSTANCE_FILE)"
 }
 
 # $1 absolute path to directory where file creation is expected
 # $2 expected filename
 function wait_file_creation() {
-    while [ 1 ]; 
-    do 
-	FILENAME=$(inotifywait --event create --format '%f' $1 2> /dev/null);
-	if [[ $FILENAME = $2 ]]; 
-	then 
-	    break; 
-	fi; 
-    done 
+	if [[ ! -f $1/$2 ]]
+	then
+    	python $BUNDLE_HOME/scripts/filename-watchdog.py $1 $2 &> /dev/null
+    fi
+}
+
+function clear() {
+    undeploy
+    remove_logs
+    exit 2
 }
 
 function undeploy() {
-    rm -rf $BUNDLE_HOME/repositories
-
     kill_process $REGISTRY_INSTANCE_FILE
     kill_process $INSTANCES_DIR/ws-eventclouds-management
-  
-    for ((i=1; i<=$NB_EVENTCLOUDS; i++));
-    do
-        kill_process $(eventcloud_instance_file $i)
-        kill_process $(ws_instance_file ws-subscribe-proxy $i)
-        kill_process $(ws_instance_file ws-publish-proxy $i)
-        kill_process $(ws_instance_file ws-putget-proxy $i)
-    done;
 
-    rm -rf $INSTANCES_DIR
-    rm -rf $OUTPUTS_DIR
+    rm -rf $INSTANCES_DIR &> /dev/null
+}
+
+function remove_logs() {
+    rm -rf $LOGS_DIR &> /dev/null
 }
 
 # $1 an absolute path to an instance file
 function kill_process() {
     if [[ -f $1 ]]
     then
-	kill -9 $(cat $1.pid) &> /dev/null
+        kill -9 $(cat $1.pid) &> /dev/null
     fi
 }
 
 main $@
 
 . $BUNDLE_HOME/scripts/configuration/unset-environment.sh
+
