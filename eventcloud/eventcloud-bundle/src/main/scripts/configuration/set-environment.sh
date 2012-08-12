41f6907c97ed0290043f416eb8faec4c65b039e9
lpellegr <lpellegr@localhost>
2012-08-12 18:27:40 +0200

    Updated infrastructure-launcher script to make it work on Mac Os X. The problem was the inotify library which is available only with a linux kernel. The script now uses watchdog, a python library to redirect monitoring operations to the underlying library supported by the operating system.
    - Some unecessary scripts have been removed
    - Added a new option to the infrastructure-launcher script
    - Fixed the proactive java policy that was not correctly loaded

--- eventcloud/eventcloud-bundle/src/main/scripts/configuration/set-environment.sh
+++ eventcloud/eventcloud-bundle/src/main/scripts/configuration/set-environment.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 
 CURRENT_DIR=$PWD/$(dirname $0)
-export BUNDLE_HOME=$CURRENT_DIR/../
-export PATH_TO_RESOURCES=$CURRENT_DIR/../resources
 
-unset CLASSPATH
+function export_variables() {
+    cd $CURRENT_DIR/../
+    export BUNDLE_HOME=$PWD
+    cd $CURRENT_DIR/../libs/
+    export PATH_TO_LIBRARIES=$PWD
+    cd $CURRENT_DIR/../resources
+    export PATH_TO_RESOURCES=$PWD
+    cd $CURRENT_DIR
+	
+    unset CLASSPATH
+    export CLASSPATH="$PATH_TO_LIBRARIES/*:${CLASSPATH}"
+    
+    debug_variables
+}
 
-# Cannot use wildcard because there is an issue when a
-# jar file contains '.' character in its name
-for jar in $(ls $CURRENT_DIR/../lib/);
-do
-    CLASSPATH="${CURRENT_DIR}/../lib/${jar}:${CLASSPATH}"
-done;
+function debug_variables() {
+    if [ $DEBUG ];
+    then
+        echo "--> Using BUNDLE_HOME=${BUNDLE_HOME}"
+        echo "--> Using PATH_TO_LIBRARIES=${PATH_TO_LIBRARIES}"
+        echo "--> Using PATH_TO_RESOURCES=${PATH_TO_RESOURCES}"
+        echo "--> Using CLASSPATH=${CLASSPATH}"
+    fi
+}
 
-export CLASSPATH
-
-if [ $DEBUG ];
-then
-    echo "--> Using BUNDLE_HOME=${BUNDLE_HOME}"
-    echo "--> Using PATH_TO_RESOURCES=${PATH_TO_RESOURCES}"
-    echo "--> Using CLASSPATH=${CLASSPATH}"
-fi
+export_variables
