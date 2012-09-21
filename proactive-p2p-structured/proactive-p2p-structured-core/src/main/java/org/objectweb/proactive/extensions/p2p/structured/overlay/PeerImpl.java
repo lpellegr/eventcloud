19e34c6be66c803b73dd832a73a1547534a1f334
lpellegr <lpellegr@localhost>
2012-09-21 17:15:03 +0200

    Closed the overlay and the underlying datastructures in PeerImpl#endComponentActivity

--- proactive-p2p-structured/proactive-p2p-structured-core/src/main/java/org/objectweb/proactive/extensions/p2p/structured/overlay/PeerImpl.java
+++ proactive-p2p-structured/proactive-p2p-structured-core/src/main/java/org/objectweb/proactive/extensions/p2p/structured/overlay/PeerImpl.java
@@ -16,6 +16,7 @@
  **/
 package org.objectweb.proactive.extensions.p2p.structured.overlay;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Collection;
@@ -124,15 +125,11 @@ public class PeerImpl extends AbstractComponent implements Peer,
      */
     @Override
     public void endComponentActivity(Body body) {
-        // TODO: enable the leave operation when it works!
-
-        // if (this.overlay.activated.get()) {
-        // try {
-        // this.leave();
-        // } catch (NetworkNotJoinedException e) {
-        // e.printStackTrace();
-        // }
-        // }
+        try {
+            this.overlay.getRequestResponseManager().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @SuppressWarnings("unused")
