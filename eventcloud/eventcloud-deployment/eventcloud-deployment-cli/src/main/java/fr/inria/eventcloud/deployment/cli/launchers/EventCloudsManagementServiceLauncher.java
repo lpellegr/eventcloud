cab4f72cbf3ef2a76ac02d005e93d08ef5191350
Bastien Sauvan <bsauvan@users.noreply.github.com>
2012-10-08 13:53:51 +0200

    Fix typo in EventCloudsManagementServiceLauncher classname.

--- eventcloud/eventcloud-deployment/eventcloud-deployment-cli/src/main/java/fr/inria/eventcloud/deployment/cli/launchers/EventCloudsManagementServiceLauncher.java
+++ eventcloud/eventcloud-deployment/eventcloud-deployment-cli/src/main/java/fr/inria/eventcloud/deployment/cli/launchers/EventCloudsManagementServiceLauncher.java
@@ -4,7 +4,7 @@ import com.beust.jcommander.Parameter;
 
 import fr.inria.eventcloud.webservices.deployment.WsDeployer;
 
-public class EventCloudsManagementServiceLaucher extends Launcher {
+public class EventCloudsManagementServiceLauncher extends Launcher {
 
     @Parameter(names = {"--registry-url", "-r"}, description = "EventClouds registry URL", required = true)
     private String registryUrl;
@@ -13,8 +13,8 @@ public class EventCloudsManagementServiceLaucher extends Launcher {
     private int port;
 
     public static void main(String[] args) {
-        EventCloudsManagementServiceLaucher launcher =
-                new EventCloudsManagementServiceLaucher();
+        EventCloudsManagementServiceLauncher launcher =
+                new EventCloudsManagementServiceLauncher();
         launcher.parseArguments(launcher, args);
         launcher.launch();
     }
