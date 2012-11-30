373f0f3cc8b86beca9727e9dfd9071f3446284da
justinerochas <justinerochas@localhost>
2012-11-30 16:04:16 +0100

    Added three test classes to evaluate the accuracy of three different broadcast algorithms over a CAN : flooding algorithm, enhanced algorithm, optimal messages overhead algorithm. Provide in addition a class to evaluate these three algorithms in one run.

---
+++ proactive-p2p-structured/proactive-p2p-structured-can/src/test/java/org/objectweb/proactive/extensions/p2p/structured/messages/can/benchmarks/FloodingBroadcast.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2011-2012 INRIA.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ **/
+package org.objectweb.proactive.extensions.p2p.structured.messages.can.benchmarks;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.objectweb.proactive.extensions.p2p.structured.deployment.CanDeploymentDescriptor;
+import org.objectweb.proactive.extensions.p2p.structured.deployment.JunitByClassCanNetworkDeployer;
+import org.objectweb.proactive.extensions.p2p.structured.logger.JobLogger;
+import org.objectweb.proactive.extensions.p2p.structured.messages.request.can.AnycastRequest;
+import org.objectweb.proactive.extensions.p2p.structured.overlay.can.StringCanOverlay;
+import org.objectweb.proactive.extensions.p2p.structured.overlay.can.zone.coordinates.CoordinateFactory;
+import org.objectweb.proactive.extensions.p2p.structured.overlay.can.zone.elements.StringElement;
+import org.objectweb.proactive.extensions.p2p.structured.providers.InjectionConstraintsProvider;
+import org.objectweb.proactive.extensions.p2p.structured.providers.SerializableProvider;
+import org.objectweb.proactive.extensions.p2p.structured.proxies.Proxies;
+import org.objectweb.proactive.extensions.p2p.structured.proxies.Proxy;
+import org.objectweb.proactive.extensions.p2p.structured.validator.can.DefaultAnycastConstraintsValidator;
+
+/**
+ * This class is suppose to provide measurements of the dissemination of
+ * a message across all peers with a basic flooding algorithm.
+ * 
+ * Note - Inspired from : 
+ * org.objectweb.proactive.extensions.p2p.structured.messages.can.AnycastLookupRequestTest
+ * 
+ * @author jrochas
+ */
+public class FloodingBroadcast extends JunitByClassCanNetworkDeployer {
+
+    private static final String LOG_FILENAME = JobLogger.PREFIX + "FloodingBroadcast.log";
+
+    private Proxy proxy;
+
+    public FloodingBroadcast() {
+        super(new CanDeploymentDescriptor<StringElement>(
+                        new SerializableProvider<StringCanOverlay>() {
+                            private static final long serialVersionUID = 130L;
+
+                            @Override
+                            public StringCanOverlay get() {
+                                return new StringCanOverlay();
+                            }
+                        }).setInjectionConstraintsProvider(InjectionConstraintsProvider.newFractalInjectionConstraintsProvider()),
+                1, JobLogger.NB_PEERS);
+    }
+    
+    public void setProxy(Proxy proxy) {
+		this.proxy = proxy;
+	}
+
+    @Override
+    public void setUp() {
+        super.setUp();
+        this.proxy = Proxies.newProxy(super.getRandomTracker()); 
+    }
+
+    @Test
+    public void measureFloodingBroadcast() throws InterruptedException {
+    	
+    	// Timestamp the beginning of the broadcast
+    	JobLogger.recordTime(LOG_FILENAME);
+    	
+    	// Sending the broadcast via an AnycastRequest (= flood)
+        this.proxy.sendv(new AnycastRequest<StringElement>(
+				new DefaultAnycastConstraintsValidator<StringElement>(
+						CoordinateFactory.newStringCoordinate())));
+
+        // The previous call is asynchronous, so
+     	// it is preferable to wait a little bit
+        Thread.sleep(2000);
+        
+        // This is going to log all the interesting metrics for a benchmark
+     	// in the LOG_FILENAME at the root of the logs directory (see JobLogger)
+        int nbPeerReached = JobLogger.logResults(this.getClass().getSimpleName(), 
+        		JobLogger.NB_PEERS, LOG_FILENAME);
+        
+        // Checking the correctness of the run
+     	Assert.assertEquals(JobLogger.NB_PEERS, nbPeerReached);
+    }
+
+    @Override
+    public void tearDown() {
+        super.tearDown();
+        try {
+            this.proxy.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+    }
+}
