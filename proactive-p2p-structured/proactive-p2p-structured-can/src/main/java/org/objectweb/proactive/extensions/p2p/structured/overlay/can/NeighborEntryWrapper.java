88a0040f1e8a28d8748247124e6eed60d0aa18ef
justinerochas <justinerochas@localhost>
2012-11-30 15:55:29 +0100

    Added wrappers of NeighborEntry and NeighborTable to handle the new broadcast algorithms.

---
+++ proactive-p2p-structured/proactive-p2p-structured-can/src/main/java/org/objectweb/proactive/extensions/p2p/structured/overlay/can/NeighborEntryWrapper.java
@@ -0,0 +1,118 @@
+package org.objectweb.proactive.extensions.p2p.structured.overlay.can;
+
+import java.io.Serializable;
+import java.util.UUID;
+import org.objectweb.proactive.extensions.p2p.structured.configuration.P2PStructuredProperties;
+import org.objectweb.proactive.extensions.p2p.structured.overlay.can.zone.elements.Element;
+
+/**
+ * An entry in an {@link NeighborTableWrapper}.
+ * 
+ * @author acraciun
+ */
+public class NeighborEntryWrapper<E extends Element> implements Serializable {
+
+	private static final long serialVersionUID = 130L;
+	private NeighborEntry<E> neighborEntry;
+	// The directions on which the peer needs to forward the broadcast request.
+	private byte[][] directions;
+	// The coordinates of the plan(if not null) that needs to be contained by
+	// the neighbors of the current peer, in order to receive the broadcast
+	// request
+	private Element[] splitPlans;
+	// The dimension relative to the sender.
+	private byte dimension;
+	// The direction relative to the sender.
+	private byte side;
+
+	public NeighborEntryWrapper(NeighborEntry<E> neighborEntry, byte dimension,
+			byte side) {
+		this.neighborEntry = neighborEntry;
+		this.dimension = dimension;
+		this.side = side;
+		this.directions = this.initializeByteArray();
+		this.splitPlans = new Element[P2PStructuredProperties.CAN_NB_DIMENSIONS
+				.getValue()];
+	}
+
+	private byte[][] initializeByteArray() {
+		byte[][] array = new byte[2][P2PStructuredProperties.CAN_NB_DIMENSIONS
+				.getValue()];
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < P2PStructuredProperties.CAN_NB_DIMENSIONS
+					.getValue(); j++) {
+				array[i][j] = -1;
+			}
+		}
+
+		return array;
+	}
+
+	public byte[][] getDirections() {
+		return directions;
+	}
+
+	public byte getDirection(int i, int j) {
+		return this.directions[i][j];
+	}
+
+	public void setDirections(byte[][] direction) {
+		this.directions = direction;
+	}
+
+	public void setDirection(int i, int dimension, byte direction) {
+		this.directions[i][dimension] = direction;
+	}
+
+	public Element[] getSplitPlans() {
+		return splitPlans;
+	}
+
+	public Element getSplitPlan(int index) {
+		return this.splitPlans[index];
+	}
+
+	public void setSplitPlans(Element[] splitPlans) {
+		this.splitPlans = splitPlans;
+	}
+
+	public void setSplitPlan(int dimension, Element plan) {
+		this.splitPlans[dimension] = plan;
+	}
+
+	public NeighborEntry<E> getNeighborEntry() {
+		return neighborEntry;
+	}
+
+	public void setNeighborEntry(NeighborEntry<E> neighborEntry) {
+		this.neighborEntry = neighborEntry;
+	}
+
+	public UUID getId() {
+		return this.neighborEntry.getId();
+	}
+
+	public byte getDimension() {
+		return dimension;
+	}
+
+	public void setDimension(byte dimension) {
+		this.dimension = dimension;
+	}
+
+	public byte getSide() {
+		return side;
+	}
+
+	public void setSide(byte side) {
+		this.side = side;
+	}
+
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		buf.append(neighborEntry);
+		return buf.toString();
+	}
+}
+
