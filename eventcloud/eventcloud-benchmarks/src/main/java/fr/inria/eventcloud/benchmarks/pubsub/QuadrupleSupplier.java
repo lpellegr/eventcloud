a0e5becffa4fee16d416a35ec13a6cb037f17cf6
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2012-12-27 17:25:56 +0100

    Copied pub/sub benchmark as a Java application

--- eventcloud/eventcloud-benchmarks/src/main/java/fr/inria/eventcloud/benchmarks/pubsub/QuadrupleSupplier.java
+++ eventcloud/eventcloud-benchmarks/src/main/java/fr/inria/eventcloud/benchmarks/pubsub/QuadrupleSupplier.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>
  **/
-package fr.inria.eventcloud.benchmarks;
+package fr.inria.eventcloud.benchmarks.pubsub;
 
 import com.google.common.base.Supplier;
 
