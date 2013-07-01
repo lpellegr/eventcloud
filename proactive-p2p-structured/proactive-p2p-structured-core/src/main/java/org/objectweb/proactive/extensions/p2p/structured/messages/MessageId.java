a67d1791f60ff79f26e14daaf3dcbceb8c33e7ea
Laurent Pellegrino <lpellegr@users.noreply.github.com>
2013-07-01 21:03:12 +0200

    Minor improvement for equals by adding reference checking

--- proactive-p2p-structured/proactive-p2p-structured-core/src/main/java/org/objectweb/proactive/extensions/p2p/structured/messages/MessageId.java
+++ proactive-p2p-structured/proactive-p2p-structured-core/src/main/java/org/objectweb/proactive/extensions/p2p/structured/messages/MessageId.java
@@ -53,6 +53,10 @@ public class MessageId implements Serializable {
      */
     @Override
     public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        
         if (obj instanceof MessageId) {
             MessageId other = (MessageId) obj;
 
