e547880bc08aeceb2df66ae7380010d9643f4f5f
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2014-04-30 15:07:24 +0200

    Updated copyright

--- eventcloud/eventcloud-core/src/main/java/fr/inria/eventcloud/proxies/EventCloudProxy.java
+++ eventcloud/eventcloud-core/src/main/java/fr/inria/eventcloud/proxies/EventCloudProxy.java
@@ -1,5 +1,5 @@
 /**
-P * Copyright (c) 2011-2013 INRIA.
+P * Copyright (c) 2011-2014 INRIA.
  * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License as published by the Free
